0f4818ca42f1c13255d4446eb08afe66ceb3bceb
abir <abirbanti527@gmail.com>
2024-03-15 12:29:43 +0530

    last update

--- script.js
+++
@@ -1,6 +0,0 @@
-function openMenu(){
-    var navlist = document.getElementById("ul_nav");
-    var btn = document.querySelector(".btn");
-    navlist.classList.toggle("active");
-    btn.classList.toggle("open");
-}
